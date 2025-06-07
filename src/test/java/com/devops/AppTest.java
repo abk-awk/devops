182ebf00cbf090cd2ff8eb1d960df6320ca48bd0
00_abk <abel.kabangu-kalambayi@efrei.net>
2025-06-08 01:35:19 +0200

    java modifcation

---
+++ src/test/java/com/devops/AppTest.java
@@ -0,0 +1,11 @@
+package com.devops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
